19fd0b080446ad7c6b9425f388791c2356c3bcfc
Lisa Brown <lisaann493@verizon.net>
2022-01-07 14:32:48 -0500

    initial commit

--- public/index.js
+++ public/index.js
@@ -1,10 +1,14 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const resetButton = document.querySelector('#reset-button');
 
-    let countValue = 0;
+    const response = await fetch('http://127.0.0.1:9001/counter');
+    const result = await response.json();
 
+    let countValue = result.value;
+    
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
@@ -15,8 +19,16 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    function reset(){
+        countValue = 0;
+        countContainer.textContent = countValue;
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    resetButton.addEventListener('click', reset);
     countContainer.textContent = countValue;
+
+    
 }
 main()
