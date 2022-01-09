b7cc9b4be663ffda54c925a19c25d85c208709d4
Lisa Brown <lisaann493@verizon.net>
2022-01-09 16:43:56 -0500

    updates for part 2

--- public/index.js
+++ public/index.js
@@ -7,28 +7,40 @@ async function main(){
     const response = await fetch('http://127.0.0.1:9001/counter');
     const result = await response.json();
 
-    let countValue = result.value;
+    let countValue = 0
     
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        updateServer()
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        updateServer()
     }
 
     function reset(){
         countValue = 0;
         countContainer.textContent = countValue;
+        updateServer()
     }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
     resetButton.addEventListener('click', reset);
     countContainer.textContent = countValue;
-
     
+    async function updateServer(){
+        await fetch('http://127.0.0.1:9001/counter', 
+        {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({'value': countValue})
+        })
+        // countValue = result
+    }
 }
+
 main()
